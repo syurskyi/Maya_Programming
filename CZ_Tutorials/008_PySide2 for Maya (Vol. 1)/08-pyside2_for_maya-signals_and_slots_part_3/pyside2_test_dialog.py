06e8f1941cdbcecdb8db86886589e761fb719d1d
Grey <sergejyurskyj@yahoo.com>
2020-08-09 16:15:26 -0700

    Committed 08-pyside2_for_maya-signals_and_slots_part_3

---
+++ CZ_Tutorials/008_PySide2 for Maya (Vol. 1)/08-pyside2_for_maya-signals_and_slots_part_3/pyside2_test_dialog.py
@@ -0,0 +1,81 @@
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+from shiboken2 import wrapInstance
+
+import maya.OpenMayaUI as omui
+
+
+def maya_main_window():
+    """
+    Return the Maya main window widget as a Python object
+    """
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window_ptr), QtWidgets.QWidget)
+
+
+class MyLineEdit(QtWidgets.QLineEdit):
+
+    enter_pressed = QtCore.Signal(str)
+
+    def keyPressEvent(self, e):
+        super(MyLineEdit, self).keyPressEvent(e)
+
+        if e.key() == QtCore.Qt.Key_Enter:
+            self.enter_pressed.emit("Enter Key Pressed")
+        elif e.key() == QtCore.Qt.Key_Return:
+            self.enter_pressed.emit("Return Key Pressed")
+
+
+class TestDialog(QtWidgets.QDialog):
+
+    def __init__(self, parent=maya_main_window()):
+        super(TestDialog, self).__init__(parent)
+
+        self.setWindowTitle("Test Dialog")
+        self.setMinimumWidth(200)
+        self.setMinimumHeight(90)
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+
+        self.create_widgets()
+        self.create_layouts()
+        self.create_connections()
+
+    def create_widgets(self):
+        self.lineedit = MyLineEdit()
+
+        self.ok_btn = QtWidgets.QPushButton("OK")
+        self.cancel_btn = QtWidgets.QPushButton("Cancel")
+
+
+    def create_layouts(self):
+        form_layout = QtWidgets.QFormLayout()
+        form_layout.addRow("Name:", self.lineedit)
+
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(self.ok_btn)
+        button_layout.addWidget(self.cancel_btn)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addLayout(form_layout)
+        main_layout.addLayout(button_layout)
+
+    def create_connections(self):
+        self.lineedit.enter_pressed.connect(self.on_enter_pressed)
+
+        self.cancel_btn.clicked.connect(self.close)
+
+    def on_enter_pressed(self, text):
+        print(text)
+
+
+if __name__ == "__main__":
+
+    try:
+        test_dialog.close() # pylint: disable=E0601
+        test_dialog.deleteLater()
+    except:
+        pass
+
+    test_dialog = TestDialog()
+    test_dialog.show()
