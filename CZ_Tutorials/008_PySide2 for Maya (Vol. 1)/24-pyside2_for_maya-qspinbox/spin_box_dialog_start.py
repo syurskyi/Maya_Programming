ef35ac6e55b7de4533d72362462cc2cf69eade79
Grey <sergejyurskyj@yahoo.com>
2020-08-09 16:39:44 -0700

    Committed 24-pyside2_for_maya-qspinbox

---
+++ CZ_Tutorials/008_PySide2 for Maya (Vol. 1)/24-pyside2_for_maya-qspinbox/spin_box_dialog_start.py
@@ -0,0 +1,57 @@
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+from shiboken2 import wrapInstance
+
+import maya.OpenMayaUI as omui
+
+
+def maya_main_window():
+    """
+    Return the Maya main window widget as a Python object
+    """
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window_ptr), QtWidgets.QWidget)
+
+
+class SpinBoxDialog(QtWidgets.QDialog):
+
+    def __init__(self, parent=maya_main_window()):
+        super(SpinBoxDialog, self).__init__(parent)
+
+        self.setWindowTitle("Spin Box Dialog")
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+
+        self.create_widgets()
+        self.create_layout()
+        self.create_connections()
+
+    def create_widgets(self):
+        self.spin_box = QtWidgets.QSpinBox()
+        self.spin_box.setFixedWidth(80)
+
+        self.double_spin_box = QtWidgets.QDoubleSpinBox()
+        self.double_spin_box.setFixedWidth(80)
+
+    def create_layout(self):
+        main_layout = QtWidgets.QFormLayout(self)
+        main_layout.addRow("Spin Box:", self.spin_box)
+        main_layout.addRow("Double Spin Box:", self.double_spin_box)
+
+    def create_connections(self):
+        self.spin_box.valueChanged.connect(self.print_value)
+        self.double_spin_box.valueChanged.connect(self.print_value)
+
+    def print_value(self, value):
+        print("Value: {0}".format(value))
+
+
+if __name__ == "__main__":
+
+    try:
+        spin_box_dialog.close() # pylint: disable=E0601
+        spin_box_dialog.deleteLater()
+    except:
+        pass
+
+    spin_box_dialog = SpinBoxDialog()
+    spin_box_dialog.show()
